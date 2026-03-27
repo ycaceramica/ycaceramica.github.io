763aef6c288ed512bdf9c2d945fe8c248f5ad3d5
ycaceramica <ycaceramica@gmail.com>
2026-03-27 18:41:34 -0300

    Update script.js

--- esmaltes/script.js
+++ esmaltes/script.js
@@ -715,7 +715,7 @@ async function descargarPDFEsmaltes(){
   doc.setTextColor(160, 150, 140)
   doc.setFontSize(7)
   doc.setFont('helvetica', 'normal')
-  doc.text('ycaceramica.github.io  |  YCA Ceramica 2026', W / 2, 293, { align: 'center' })
+  doc.text('ycaceramica.com.ar  |  YCA Ceramica 2026', W / 2, 293, { align: 'center' })
 
   doc.save('YCA_Ceramica_Esmaltes.pdf')
 }
@@ -794,7 +794,7 @@ async function descargarPDFDensidad(){
   doc.setTextColor(160, 150, 140)
   doc.setFontSize(7)
   doc.setFont('helvetica', 'normal')
-  doc.text('ycaceramica.github.io  |  YCA Ceramica 2026', W / 2, 293, { align: 'center' })
+  doc.text('ycaceramica.com.ar  |  YCA Ceramica 2026', W / 2, 293, { align: 'center' })
 
   doc.save('YCA_Ceramica_Densidad.pdf')
 }
